61107b1e73201166af7f88f0989462b52a673a59
tanphat12344 <letrantanphat160998@gmail.com>
2022-03-12 09:51:34 +0700

    reponsive tat ca

--- w3portfolio.js
+++ w3portfolio.js
@@ -1,12 +1,14 @@
-const menuButton = document.querySelector(".icon-menu");
+const menuButton = document.querySelector(".menu1");
 const menu = document.querySelector(".navbar__links");
 const closeBtn = document.querySelectorAll(".navbar__link");
 
 
 const overlay = document.querySelector("#overlay");
 
-
-
+const overlayimg = document.querySelector(".overlayimg");
+const imgs = document.querySelectorAll(".imgheader img");
+const imgBlock = document.querySelector(".imgBlock");
+console.log(overlayimg);
 menuButton.addEventListener("click", () => {
     menu.classList.toggle("navbar__open");
     overlay.classList.toggle("show");
@@ -23,13 +25,18 @@ closeBtn.forEach((item)=>{
 overlay.addEventListener("click", () =>{
     overlay.classList.toggle("show");
     menu.classList.toggle("navbar__open");
-
-
     // overlay.classList.remove("show");
     // menu.classList.remove("navbar__open");
-
 });
 
+// imgs.forEach(function(img){
+//     img.addEventListener("click", function(){
+//         console.log(img.src)
+//         // overlayimg.style.display="block";
+//         imgBlock.innerHTML = `<img src="${img.src}" alt="image">`
+        
+//     })
+// })
 
 
 
